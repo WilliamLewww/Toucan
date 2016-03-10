29b5b8516c31b8957ebf8b866784842194557b2d
WilliamLewww <WilliamLew32@gmail.com>
2016-03-09 23:10:19 -0800

    incorporated sdl with winsock

--- Toucan/main.cpp
+++ Toucan/main.cpp
@@ -1,5 +1,8 @@
 #include <winsock2.h>
 #include <iostream>
+#include <SDL.h>
+#include <SDL_opengl.h>
+#include "main.h"
 
 #pragma comment(lib, "ws2_32.lib")
 
@@ -7,13 +10,24 @@
 #define BUFLEN 512
 #define PORT 27861
 
-int main(void)
-{
-	struct sockaddr_in si_other;
-	int s, slen = sizeof(si_other);
-	char buf[BUFLEN];
-	char message[BUFLEN];
-	WSADATA wsa;
+void Update(int gameTime);
+void Render(SDL_Window* window, SDL_GLContext context);
+char * SendMessage(char message[]);
+
+WSADATA wsa;
+SDL_Event event;
+SDL_GLContext context;
+
+struct sockaddr_in si_other;
+int s, slen = sizeof(si_other);
+char buf[BUFLEN];
+
+bool isRunning = true;
+int frameStart, frameEnd, deltaTime = 0;
+int main(int argc, char *argv[]) {
+	displayWindow = SDL_CreateWindow("Mallard", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREENWIDTH, SCREENHEIGHT, SDL_WINDOW_OPENGL);
+	context = SDL_GL_CreateContext(displayWindow);
+	glOrtho(-SCREENWIDTH / 2, SCREENWIDTH / 2, SCREENHEIGHT / 2, -SCREENHEIGHT / 2, 0, 1);
 
 	WSAStartup(MAKEWORD(2, 2), &wsa);
 	s = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
@@ -23,19 +37,46 @@ int main(void)
 	si_other.sin_port = htons(PORT);
 	si_other.sin_addr.S_un.S_addr = inet_addr(SERVER);
 
-	while (1)
-	{
-		printf("Enter message : ");
-		std::cin >> (message);
+	while (isRunning) {
+		while (SDL_PollEvent(&event)) {
+			if (event.type == SDL_QUIT)
+				isRunning = false;
+		}
 
-		sendto(s, message, strlen(message), 0, (struct sockaddr *) &si_other, slen);
-		memset(buf, '\0', BUFLEN);
-		recvfrom(s, buf, BUFLEN, 0, (struct sockaddr *) &si_other, &slen);
-		std::cout << buf << std::endl;
+		if (deltaTime < 1) {
+			frameStart = SDL_GetTicks();
+			SDL_Delay(1);
+			frameEnd = SDL_GetTicks();
+			deltaTime = frameEnd - frameStart;
+		}
+		frameStart = SDL_GetTicks();
+		Update(deltaTime);
+		Render(displayWindow, context);
+		frameEnd = SDL_GetTicks();
+		deltaTime = frameEnd - frameStart;
 	}
 
 	closesocket(s);
 	WSACleanup();
 
 	return 0;
+}
+
+void Update(int gameTime) {
+
+}
+
+void Render(SDL_Window* window, SDL_GLContext context) {
+	SDL_GL_MakeCurrent(window, context);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glMatrixMode(GL_PROJECTION);
+
+	SDL_GL_SwapWindow(window);
+}
+
+char * SendMessage(char message[]) {
+	sendto(s, message, strlen(message), 0, (struct sockaddr *) &si_other, slen);
+	memset(buf, '\0', BUFLEN);
+	recvfrom(s, buf, BUFLEN, 0, (struct sockaddr *) &si_other, &slen);
+	return buf;
 }
---
+++ Toucan/main.h
@@ -0,0 +1,4 @@
+#pragma once
+
+static const int SCREENWIDTH = 800, SCREENHEIGHT = 600;
+static SDL_Window* displayWindow;
